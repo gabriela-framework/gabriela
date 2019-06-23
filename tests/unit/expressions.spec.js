ca9b9444ab6ffdb940161f7c169212c4d7262cd6
mario <likearollingstone199@gmail.com>
2019-06-23 11:59:03 +0100

    Added simple expression parsing in order to use it in middleware dependency injectable services

---
+++ tests/unit/expressions.spec.js
@@ -0,0 +1,38 @@
+const mocha = require('mocha');
+const chai = require('chai');
+
+const it = mocha.it;
+const describe = mocha.describe;
+const expect = chai.expect;
+
+const parseExpression = require('../../gabriela/expression/parse');
+
+describe('Expressions tests', () => {
+    it('should return function name and function arguments', () => {
+        const expression = 'fn(arg1, arg2, arg3)';
+
+        const result = parseExpression(expression);
+
+        expect(result).to.have.property('fnName');
+        expect(result).to.have.property('dependencies');
+
+        expect(result.fnName).to.be.a('string');
+        expect(result.dependencies).to.be.a('array');
+
+        expect(result.dependencies.length).to.be.equal(3);
+    });
+
+    it('should return function name and an empty array of function arguments', () => {
+        const expression = 'fn()';
+
+        const result = parseExpression(expression);
+
+        expect(result).to.have.property('fnName');
+        expect(result).to.have.property('dependencies');
+
+        expect(result.fnName).to.be.a('string');
+        expect(result.dependencies).to.be.a('array');
+
+        expect(result.dependencies.length).to.be.equal(0);
+    });
+});
---
+++ tests/unit/failingExpressionParsing.spec.js
@@ -0,0 +1,25 @@
+const mocha = require('mocha');
+const chai = require('chai');
+
+const it = mocha.it;
+const describe = mocha.describe;
+const expect = chai.expect;
+
+const parseExpression = require('../../gabriela/expression/parse');
+
+describe('Failing expression parsing tests', () => {
+    it('should fail to parse an invalid function expression', () => {
+        const fn = 'arg = 0';
+
+        let exceptionEntered = false;
+        try {
+            parseExpression(fn);
+        } catch (e) {
+            expect(e.message).to.be.equal(`Invalid expression parsing. Cannot recognize function name in expression '${fn}'`);
+
+            exceptionEntered = true;
+        }
+
+        expect(exceptionEntered).to.be.equal(true);
+    });
+});
