89f6af8d9cbf7b570fdc036899ffabceaea1773a
mario <likearollingstone199@gmail.com>
2019-06-26 10:11:57 +0100

    Created a failing test for module framework events

--- tests/unit/frameworkEvents.spec.js
+++ tests/unit/frameworkEvents.spec.js
@@ -1,5 +1,6 @@
 const mocha = require('mocha');
 const chai = require('chai');
+const assert = require('assert');
 
 const it = mocha.it;
 const describe = mocha.describe;
@@ -9,16 +10,28 @@ const gabriela = require('../../gabriela/gabriela');
 
 describe('Framework events', () => {
     it('should execute a named module start event', () => {
+        let onModuleStarted = false;
+        let onModuleFinished = false;
+
         const module = {
             name: 'module',
             events: {
                 onModuleStarted: function() {
-
+                    onModuleStarted = true;
                 },
                 onModuleFinished: function() {
-
+                    onModuleFinished = true;
                 }
             }
-        }
+        };
+
+        const g = gabriela.asRunner();
+
+        g.addModule(module);
+
+        return g.runModule().then(() => {
+            expect(onModuleStarted).to.be.equal(true);
+            expect(onModuleFinished).to.be.equal(true);
+        });
     });
 });
