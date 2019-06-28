703856a6e9beefebc1d6e317d00ee013f6f4a9c4
mario <likearollingstone199@gmail.com>
2019-06-28 10:49:27 +0100

    Extracted private _resolveDependency() function into a module resolveDependencies

--- gabriela/module/middleware/runMiddleware.js
+++ gabriela/module/middleware/runMiddleware.js
@@ -1,26 +1,9 @@
 const taskRunnerFactory = require('../../misc/taskRunner');
 const {asyncFlowTypes} = require('../../misc/types');
 const _waitCheck = require('../../util/_waitCheck');
+const resolveDependencies = require('../../dependencyInjection/resolveDependencies');
 
 const {createGenerator, getArgs, wait, inArray} = require('../../util/util');
-
-function _resolveDependency(mdl, name, config) {
-    if (mdl.compiler.has(name)) {
-        return mdl.compiler.compile(name, mdl.compiler, config);
-    } else if (mdl.sharedCompiler.has(name)) {
-        const definition = mdl.sharedCompiler.getOwnDefinition(name);
-
-        // if it is shared with a module name
-        if (definition.isSharedWith(mdl.name)) {
-            return mdl.sharedCompiler.compile(name, mdl.sharedCompiler, config);
-        }
-
-        // if it is shared with a module that is in a plugin with name mdl.plugin.name
-        if (mdl.isInPlugin() && definition.isSharedWith(mdl.plugin.name)) {
-            return mdl.sharedCompiler.compile(name, mdl.sharedCompiler, config);
-        }
-    }
-}
 
 async function recursiveMiddlewareExec(exec, taskRunner, mdl, state, config, generator) {
     const args = getArgs(exec, {
@@ -31,7 +14,7 @@ async function recursiveMiddlewareExec(exec, taskRunner, mdl, state, config, gen
     });
 
     exec.call(this, ...args.map((val) => {
-        const dep = _resolveDependency(mdl, val.name, config);
+        const dep = resolveDependencies(mdl, val.name, config);
 
         if (dep) return dep;
 
