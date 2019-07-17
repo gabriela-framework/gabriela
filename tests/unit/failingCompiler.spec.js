ad3eb28f04103eb23f90b563cd485ed5192e4584
mario <likearollingstone199@gmail.com>
2019-07-17 16:35:39 +0100

    Moved specific failing test only to concrete compiler instance

---
+++ tests/unit/failingCompiler.spec.js
@@ -0,0 +1,165 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const requestPromise = require('request-promise');
+const assert = require('assert');
+
+const it = mocha.it;
+const describe = mocha.describe;
+const expect = chai.expect;
+
+const Compiler = require('../../gabriela/dependencyInjection/compiler');
+
+describe('Failing concrete compiler tests', () => {
+    it('should fail to compile a dependency because init.init does not return a function', () => {
+        const userServiceInit = {
+            name: 'userService',
+            init: function() {
+                // returns nothing
+            }
+        };
+
+        const compiler = Compiler.create();
+
+        compiler.add(userServiceInit);
+
+        let entersException = false;
+        try {
+            compiler.compile('userService');
+        } catch (err) {
+            entersException = true;
+
+            expect(err.message).to.be.equal(`Dependency injection error. Target service userService cannot return a falsy value`)
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+
+    it('should fail to get an own definition object', () => {
+        const userServiceInit = {
+            name: 'userService',
+            init: function() {
+                // returns nothing
+            }
+        };
+
+        const compiler = Compiler.create();
+
+        compiler.add(userServiceInit);
+
+        let entersException = false;
+        try {
+            compiler.getOwnDefinition('nonExistent');
+        } catch (err) {
+            entersException = true;
+
+            expect(err.message).to.be.equal(`Dependency injection error. Definition object with name 'nonExistent' not found`);
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+
+    it('should fail to compile a dependency because of non existent service', () => {
+        let entersException = false;
+
+        const compiler = Compiler.create();
+
+        try {
+            compiler.compile('nonExistentService');
+        } catch (err) {
+            entersException = true;
+
+            expect(err.message).to.be.equal(`Dependency injection error. 'nonExistentService' not found in the dependency tree`)
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+
+    it('should fail to compile a dependency because of invalid dependency name', () => {
+        let entersException = false;
+
+        const compiler = Compiler.create();
+
+        let invalidService = {
+            name: 'name',
+            scope: 'module',
+            init: function() {
+                function initService() {}
+
+                return new initService();
+            }
+        };
+
+        compiler.add(invalidService);
+
+        try {
+            compiler.compile(1);
+        } catch (err) {
+            entersException = true;
+
+            expect(err.message).to.be.equal(`Dependency injection error. 'compile' method expect a string as a name of a dependency that you want to compile`);
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+
+    it('should fail the compiler pass if the client code tries to use the compile() method inside a compiler pass init function', () => {
+        const userServiceInit = {
+            name: 'userService',
+            compilerPass: {
+                init: function(config, compiler) {
+                    compiler.compile('userRepository')
+                },
+                property: null,
+            },
+            init: function() {
+                return () => {};
+            }
+        };
+
+        const compiler = Compiler.create();
+
+        compiler.add(userServiceInit);
+
+        let entersException = false;
+        try {
+            compiler.compile('userService');
+        } catch(e) {
+            entersException = true;
+
+            expect(e.message).to.be.equal(`Dependency injection error in service '${userServiceInit.name}'. Compiling inside a compiler pass is forbidden`);
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+
+    it('should fail to compile if a property is not found in the config for usage in a compiler pass', () => {
+        const userServiceInit = {
+            name: 'userService',
+            compilerPass: {
+                init: function(config, compiler) {
+                },
+                property: 'nonExistent',
+            },
+            init: function() {
+                return () => {};
+            }
+        };
+
+        const compiler = Compiler.create();
+
+        compiler.add(userServiceInit);
+
+        let entersException = false;
+        try {
+            compiler.compile('userService', compiler, {
+                validation: {}
+            });
+        } catch(e) {
+            entersException = true;
+
+            expect(e.message).to.be.equal(`Dependency injection error in a compiler pass in service '${userServiceInit.name}'. Property 'nonExistent' does not exist in config`);
+        }
+
+        expect(entersException).to.be.equal(true);
+    });
+});
--- tests/unit/failingDependencyInjection.spec.js
+++ tests/unit/failingDependencyInjection.spec.js
@@ -12,70 +12,6 @@ const Compiler = require('../../gabriela/dependencyInjection/compiler');
 const config = require('../config/config');
 
 describe('Failing dependency injection tests', () => {
-    it('should fail to compile a dependency because init.init does not return a function', () => {
-        const userServiceInit = {
-            name: 'userService',
-            init: function() {
-                // returns nothing
-            }
-        };
-
-        const compiler = Compiler.create();
-
-        compiler.add(userServiceInit);
-
-        let entersException = false;
-        try {
-            compiler.compile('userService');
-        } catch (err) {
-            entersException = true;
-
-            expect(err.message).to.be.equal(`Dependency injection error. Target service userService cannot return a falsy value`)
-        }
-
-        expect(entersException).to.be.equal(true);
-    });
-
-    it('should fail to get an own definition object', () => {
-        const userServiceInit = {
-            name: 'userService',
-            init: function() {
-                // returns nothing
-            }
-        };
-
-        const compiler = Compiler.create();
-
-        compiler.add(userServiceInit);
-
-        let entersException = false;
-        try {
-            compiler.getOwnDefinition('nonExistent');
-        } catch (err) {
-            entersException = true;
-
-            expect(err.message).to.be.equal(`Dependency injection error. Definition object with name 'nonExistent' not found`);
-        }
-
-        expect(entersException).to.be.equal(true);
-    });
-
-    it('should fail to compile a dependency because of non existent service', () => {
-        let entersException = false;
-
-        const compiler = Compiler.create();
-
-        try {
-            compiler.compile('nonExistentService');
-        } catch (err) {
-            entersException = true;
-
-            expect(err.message).to.be.equal(`Dependency injection error. 'nonExistentService' not found in the dependency tree`)
-        }
-
-        expect(entersException).to.be.equal(true);
-    });
-
     it('should fail to compile because init dependency value not being an object', () => {
         let entersException = false;
 
@@ -253,34 +189,6 @@ describe('Failing dependency injection tests', () => {
 
             expect(err.message).to.be.equal(`Dependency injection error. Invalid service init for dependency with name 'userService'. If a dependency is marked as asynchronous with 'isAsync' option, it has to include 'next' function in the argument list and call it when service construction is ready`);
         });
-    });
-
-    it('should fail to compile a dependency because of invalid dependency name', () => {
-        let entersException = false;
-
-        const compiler = Compiler.create();
-
-        let invalidService = {
-            name: 'name',
-            scope: 'module',
-            init: function() {
-                function initService() {}
-
-                return new initService();
-            }
-        };
-
-        compiler.add(invalidService);
-
-        try {
-            compiler.compile(1);
-        } catch (err) {
-            entersException = true;
-
-            expect(err.message).to.be.equal(`Dependency injection error. 'compile' method expect a string as a name of a dependency that you want to compile`);
-        }
-
-        expect(entersException).to.be.equal(true);
     });
 
     it('should fail to compile a dependency if the dependency data type is not an object', () => {
@@ -680,67 +588,6 @@ describe('Failing dependency injection tests', () => {
             expect(e.message).to.be.equal(`Dependency injection error for '${userServiceInit.name}' in module 'module'. 'compilerPass' option must be an object with property 'init' that is required and must be a function and 'property' that is optional and must be a string`);
 
             entersException = true;
-        }
-
-        expect(entersException).to.be.equal(true);
-    });
-
-    it('should fail the compiler pass if the client code tries to use the compile() method inside a compiler pass init function', () => {
-        const userServiceInit = {
-            name: 'userService',
-            compilerPass: {
-                init: function(config, compiler) {
-                    compiler.compile('userRepository')
-                },
-                property: null,
-            },
-            init: function() {
-                return () => {};
-            }
-        };
-
-        const compiler = Compiler.create();
-
-        compiler.add(userServiceInit);
-
-        let entersException = false;
-        try {
-            compiler.compile('userService');
-        } catch(e) {
-            entersException = true;
-
-            expect(e.message).to.be.equal(`Dependency injection error in service '${userServiceInit.name}'. Compiling inside a compiler pass is forbidden`);
-        }
-
-        expect(entersException).to.be.equal(true);
-    });
-
-    it('should fail to compile if a property is not found in the config for usage in a compiler pass', () => {
-        const userServiceInit = {
-            name: 'userService',
-            compilerPass: {
-                init: function(config, compiler) {
-                },
-                property: 'nonExistent',
-            },
-            init: function() {
-                return () => {};
-            }
-        };
-
-        const compiler = Compiler.create();
-
-        compiler.add(userServiceInit);
-
-        let entersException = false;
-        try {
-            compiler.compile('userService', compiler, {
-                validation: {}
-            });
-        } catch(e) {
-            entersException = true;
-
-            expect(e.message).to.be.equal(`Dependency injection error in a compiler pass in service '${userServiceInit.name}'. Property 'nonExistent' does not exist in config`);
         }
 
         expect(entersException).to.be.equal(true);
