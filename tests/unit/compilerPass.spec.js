f9e61961b931aa306b110d5009db20b198afa27c
mario <likearollingstone199@gmail.com>
2019-07-17 16:31:36 +0100

    Added test that ensures that compiler passed to compiler pass cannot set any properties of the compiler

--- tests/unit/compilerPass.spec.js
+++ tests/unit/compilerPass.spec.js
@@ -61,4 +61,45 @@ describe('Compiler pass tests', () => {
             expect(entersCompilerPass).to.be.equal(true);
         });
     });
+
+    it('should assert that compiler argument passed to compiler pass cannot set any properties on that compiler', () => {
+        let entersCompilerPass = false;
+        let entersMiddleware = false;
+
+        const userService = {
+            name: 'userService',
+            compilerPass: {
+                init: function(config, compiler) {
+                    entersCompilerPass = true;
+
+                    compiler.hasOwn = null;
+
+                    expect(compiler.hasOwn).to.be.a('function');
+                },
+            },
+            init: function() {
+                function UserService() {}
+
+                return new UserService();
+            }
+        };
+
+        const g = gabriela.asProcess(config);
+
+        g.addModule({
+            name: 'module',
+            dependencies: [userService],
+            moduleLogic: [function(userService) {
+                entersMiddleware = true;
+
+                expect(userService).to.be.a('object');
+
+            }]
+        });
+
+        return g.runModule().then(() => {
+            expect(entersMiddleware).to.be.equal(true);
+            expect(entersCompilerPass).to.be.equal(true);
+        });
+    });
 });
