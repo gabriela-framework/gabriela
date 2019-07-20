3de5c97b9fb4e57dc02224d079ef28293cd114f9
mario <likearollingstone199@gmail.com>
2019-07-20 11:24:32 +0100

    Removed special argument for resolving arguments since that can be better resolved outside getArgs function

--- gabriela/util/util.js
+++ gabriela/util/util.js
@@ -81,12 +81,7 @@ function getArgs(fn, values, specialCb) {
                 name: arg,
                 value: null,
             });
-        } else if (!hasKey(values, arg) && specialCb) {
-            args.push({
-                name: 'special',
-                value: specialCb.call(null, ...[arg]),
-            });
-        } else {
+        }else {
             args.push({
                 name: arg,
                 value: values[arg],
