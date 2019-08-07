42d7d196f7d59ded9c0c29cee1b60fedb97456a8
mario <likearollingstone199@gmail.com>
2019-08-07 17:46:24 +0100

    Made the collection more generic

--- src/gabriela/misc/collection.js
+++ src/gabriela/misc/collection.js
@@ -1,24 +1,29 @@
 const deepCopy = require('deepcopy');
-const {hasKey} = require('../util/util');
+const {hasKey, is} = require('../util/util');
 
+/**
+ * A generic collection of objects. Every entry in the collection has to have a 'name' property.
+ */
 function factory() {
     this.create = function() {
         return new instance();
     };
 
     function instance() {
-        const modules = {};
+        const entries = {};
 
-        function add(mdl) {
-            if (has(mdl.name)) {
-                throw new Error(`Invalid module. Module with name '${mdl.name}' already exists`);
+        function add(entry) {
+            if (!entry.name || !is('string', entry.name)) throw new Error(`Invalid collection entry. Every collection entry must have a name property`);
+
+            if (has(entry.name)) {
+                throw new Error(`Invalid module. Module with name '${entry.name}' already exists`);
             }
 
-            modules[mdl.name] = deepCopy(mdl);
+            entries[entry.name] = deepCopy(entry);
         }
     
         function has(name) {
-            return hasKey(modules, name);
+            return hasKey(entries, name);
         }
     
         function get(name) {
@@ -26,28 +31,28 @@ function factory() {
                 return undefined;
             }
 
-            return deepCopy(modules[name]);
+            return deepCopy(entries[name]);
         }
 
         function getAll() {
             // todo: replace this with reference safe handling because the collection cannot return
             // the not-copied references
-            return modules;
+            return entries;
         }
         
         function remove(name) {
             if (!has(name)) return false;
     
             // add code for non configurable properties or a try/catch if in strict mode
-            delete modules[name];
+            delete entries[name];
     
             return true;
         }
 
-        function replace(mdl) {
-            if (!has(mdl.name)) return;
+        function replace(entry) {
+            if (!has(entry.name)) return;
 
-            modules[mdl.name] = mdl;
+            entries[entry.name] = entry;
         }
 
         this.add = add;
