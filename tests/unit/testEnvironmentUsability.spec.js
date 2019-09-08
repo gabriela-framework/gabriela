5cd14811283910b84dc2eae95d8056dafea69542
mario <likearollingstone199@gmail.com>
2019-09-08 15:11:52 +0100

    Added basic usability tests for dependency injection testing environment

--- tests/unit/testEnvironmentUsability.spec.js
+++ tests/unit/testEnvironmentUsability.spec.js
@@ -8,6 +8,47 @@ const expect = chai.expect;
 const gabriela = require('../../src/gabriela/gabriela');
 const config = require('../config/config');
 
-describe('Test environment usability tests', () => {
-    
+describe('Test environment usability tests', function() {
+    it('should instantiate a basic dependency tree', () => {
+        const testApp = gabriela.asTest(config);
+
+        const userRepository = {
+            name: 'userRepository',
+            init: function() {
+                function UserRepository() {
+
+                }
+
+                return new UserRepository();
+            }
+        };
+
+        const userService = {
+            name: 'userService',
+            init: function(userRepository) {
+                function UserService() {
+                    this.userRepository = userRepository;
+                    this.createUser = function() {};
+                    this.removeUser = function() {};
+                }
+
+                return new UserService();
+            }
+        };
+
+        const resolvedUserService = testApp
+            .loadDependency([userService, userRepository])
+            .resolve('userService');
+
+        expect(resolvedUserService).to.be.a('object');
+        expect(resolvedUserService.createUser).to.be.a('function');
+        expect(resolvedUserService.removeUser).to.be.a('function');
+        expect(resolvedUserService.userRepository).to.be.a('object');
+
+        const resolvedUserRepository = testApp
+            .loadDependency([userService, userRepository])
+            .resolve('userRepository');
+
+        expect(resolvedUserRepository).to.be.a('object');
+    });
 });
